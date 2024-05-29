36421174fc623718efb83554e434210e3db240ec
xxliao100 <xxliao100@163.com>
2024-05-29 18:47:05 +0800

    数据结构代码提交

---
+++ src/main/java/com/xxliao/datastructure/graph/Edge.java
@@ -0,0 +1,9 @@
+package com.xxliao.datastructure.graph;
+/** 
+ * @description: TODO
+ * @author xxliao
+ * @date 2024/5/29 16:57
+ */
+
+public class Edge {
+}
---
+++ src/main/java/com/xxliao/datastructure/graph/Graph.java
@@ -0,0 +1,17 @@
+package com.xxliao.datastructure.graph.adjacency_matrix;
+
+import java.util.List;
+
+/**
+ * @author xxliao
+ * @description: 邻接举证实现图
+ * @date 2024/5/29 16:00
+ */
+public class Graph {
+
+    // 存储顶点容器
+    private List vertexList;
+    // 邻接矩阵，用来存储边信息
+    private int[][] edges;
+    
+}
